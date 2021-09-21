c985799624063ed4db87753c57559817ec1d7cb0
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-09-21 10:36:46 +0900

    Update 20210915061006_create_rooms.rb

--- db/migrate/20210915061006_create_rooms.rb
+++ db/migrate/20210915061006_create_rooms.rb
@@ -5,7 +5,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
-    end
   end
 end
