7eb9d655c7cb9089d9a306c7067ef1affa4f909d
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-09-22 15:19:21 +0900

    syuusei

--- db/migrate/20210915061006_create_rooms.rb
+++ db/migrate/20210915061006_create_rooms.rb
@@ -1,10 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      ## Database authenticatable
-      t.string :name,               null: false
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+    create_table :rooms do |t|
+      t.string :name, null: false
+      t.timestamps
     end
   end
 end
