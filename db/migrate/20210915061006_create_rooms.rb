3d76c6cf6fef2f1ef127bb1bb7d2e1cd88a0516a
KeisukeKKKK <90385361+KeisukeKKKK@users.noreply.github.com>
2021-09-22 11:00:13 +0900

    git merge --abort

--- db/migrate/20210915061006_create_rooms.rb
+++ db/migrate/20210915061006_create_rooms.rb
@@ -5,5 +5,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+    end
   end
 end
